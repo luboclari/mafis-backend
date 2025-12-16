82ee4f7ae960bd49a15125f14bb75f83758442d1
luboclari <luboclari@gmail.com>
2025-12-16 01:26:03 -0500

    Update db.py

--- db.py
+++ db.py
@@ -4,9 +4,9 @@ import os
 
 def get_connection():
     return pymysql.connect(
-        host=os.environ.get('DB_HOST', 'localhost'),  # << Lee la clave DB_HOST
+        host=os.environ.get('DB_HOST', 'shinkansen.proxy.rlwy.net'),  # << Lee la clave DB_HOST
         user=os.environ.get('DB_USER', 'root'),      # << Lee la clave DB_USER
-        password=os.environ.get('DB_PASSWORD', ''),  # << Lee la clave DB_PASSWORD
+        password=os.environ.get('DB_PASSWORD', 'jteoApphwrVEJklHMncvmwOweZhQLgsY'),  # << Lee la clave DB_PASSWORD
         database=os.environ.get('DB_NAME', 'activos'), # << Lee la clave DB_NAME
         # ...
     )
